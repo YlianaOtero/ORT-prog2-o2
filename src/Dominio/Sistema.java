f617c679a09f6ed1a0bd5ba3dc3a532c9d9e894b
Yliana <ylianaotero@gmail.com>
2022-11-06 20:10:48 -0300

    Termine los metodos de cargas.

--- src/Dominio/Sistema.java
+++ src/Dominio/Sistema.java
@@ -11,61 +11,95 @@ import java.util.ArrayList;
 
 /** Clase principal del programa.
  * @author yliana*/
-public abstract class Sistema implements Serializable {
-    private static Inventario inventario;
-    private static ListaDrones drones;
-    private static Personal personal;
-    private static ArrayList<Carga> cargas;
-    private static ArrayList<Vuelo> vuelos;
-    public static boolean sistemaPrecargado = false;
+public class Sistema implements Serializable {
+    private Inventario inventario;
+    private ListaDrones drones;
+    private Personal personal;
+    private ListaCargas cargas;
+    private ListaVuelos vuelos;
+    public ObjectInputStream in;
     
-    public static void main(String[] args) {
+    /** Crea un objeto de tipo Sistema.
+     * @param sistemaPrecargado si es True, se debe intentar cargar los datos del sistema desde archivos existentes.
+     * Si es False, es debe crear todo desde cero.*/
+    public Sistema(boolean sistemaPrecargado) {
         if (sistemaPrecargado) {
-            
+            cargarSistema();
         } else {
             inicializarDeCero();
         }
-        //ACA TIENE QUE CREARSE LA VENTANA DE MENU PRINCIPAL!!
+    }
+
+    /** Se cargan todos los elementos del Sistema desde archivos preexistentes. */
+    public void cargarSistema() {
+        cargarInventario();
+        cargarPersonal();
+        cargarDrones(); 
+        cargarCargas();
+        cargarVuelos();
     }
     
-    public static void inicializarDeCero() {
+    /** Se cargan todos los elementos el sistema desde un estado limpio.*/
+    public void inicializarDeCero() {
         inventario = new Inventario();
         drones = new ListaDrones();
         personal = new Personal();
-        cargas = new ArrayList<Carga>();
-        vuelos = new ArrayList<Vuelo>();
+        cargas = new ListaCargas();
+        vuelos = new ListaVuelos();
     }
-    
-    public static void cargarSistema() {
-        ObjectInputStream in;
+
+    /** Se intenta cargar el inventario desde un archivo preexistente. En caso de
+     * que no se encuentre el archivo, se carga el inventario desde cero.*/
+    public void cargarInventario() {
         try {
             in = new ObjectInputStream(new FileInputStream("articulos"));
             inventario = (Inventario)in.readObject();
-        } catch (FileNotFoundException ex) {
-            inventario = new Inventario();
         } catch (IOException | ClassNotFoundException ex) {
             inventario = new Inventario();
         }
-        
-        try {
-            in = new ObjectInputStream(new FileInputStream("drones"));
-            drones = (ListaDrones)in.readObject();
-        } catch (FileNotFoundException ex) {
-            drones = new ListaDrones();
-        } catch (IOException | ClassNotFoundException ex) {
-            drones = new ListaDrones();
-        }
-        
+    }
+
+    /** Se intenta cargar el personal desde un archivo preexistente. En caso de
+     * que no se encuentre el archivo, se carga el personal desde cero.*/
+    public void cargarPersonal() {
         try {
             in = new ObjectInputStream(new FileInputStream("funcionarios"));
             personal = (Personal)in.readObject();
-        } catch (FileNotFoundException ex) {
-            personal = new Personal();
         } catch (IOException | ClassNotFoundException ex) {
             personal = new Personal();
         }
-        
-        
     }
-    
+
+    /** Se intenta cargar la lista de drones desde un archivo preexistente. 
+     * En caso de que no se encuentre el archivo, se carga la lista desde cero.*/
+    public void cargarDrones() {
+        try {
+            in = new ObjectInputStream(new FileInputStream("drones"));
+            drones = (ListaDrones)in.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            drones = new ListaDrones();
+        }
+    }
+
+    /** Se intenta cargar la lista de cargas desde un archivo preexistente. 
+     * En caso de que no se encuentre el archivo, se carga la lista desde cero.*/
+    public void cargarCargas() {
+        try {
+            in = new ObjectInputStream(new FileInputStream("cargas"));
+            cargas = (ListaCargas)in.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            cargas = new ListaCargas();
+        }
+    }
+
+    /** Se intenta cargar la lista de vuelos desde un archivo preexistente. 
+     * En caso de que no se encuentre el archivo, se carga la lista desde cero.*/
+    public void cargarVuelos() {
+        try {
+            in = new ObjectInputStream(new FileInputStream("vuelos"));
+            vuelos = (ListaCargas)in.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            vuelos = new ListaVuelos();
+        }
+    }
 }
