3190ffbf67ec71719bc002853efcf1cded59a9f9
Yliana <ylianaotero@gmail.com>
2022-11-06 19:36:47 -0300

    Corregi nombre y package
    Le arregle el nombre  de INicio a Inicio.
    Cree el package Inicio y movi la clase para ahi.

---
+++ src/Inicio/Inicio.java
@@ -0,0 +1,16 @@
+
+package Inicio;
+import Dominio.*;
+import Interfaz.*;
+/**
+ *
+ * @author natalia
+ */
+public class Inicio {
+    public static void main(String[] args) {
+        VentanaInicial ventanaIni = new VentanaInicial();
+        //cree la ventana de inicio (la de la clase VentanaInicial.java)
+        ventanaIni.setVisible(true);
+        //la hago visible
+    }    
+}
