553dfece44e2cc13f87ede2389b958dba274f497
Yliana <ylianaotero@gmail.com>
2022-11-07 00:03:02 -0300

    Ajustes en las pruebas

--- src/Interfaz/PruebaArticulo.java
+++ src/Interfaz/PruebaArticulo.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Interfaz;
 
-import Dominio.Inventario;
 import Dominio.Sistema;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
--- src/Interfaz/PruebaFuncionario.java
+++ src/Interfaz/PruebaFuncionario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Interfaz;
 
 import Dominio.Sistema;
@@ -18,6 +14,5 @@ public class PruebaFuncionario extends IngresoFuncionario {
 
         IngresoFuncionario nueva = new IngresoFuncionario(datos.getPersonal());
         nueva.setVisible(true);
-        
     }
 }
