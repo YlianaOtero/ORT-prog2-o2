338a7a010fca65360a09d3775b1fe37115c6dd10
Yliana <ylianaotero@gmail.com>
2022-11-21 00:33:28 -0300

    Emprolijé el código de las ventanas y agregué comentarios descriptivos.

--- src/Interfaz/IngresoFuncionario.java
+++ src/Interfaz/IngresoFuncionario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package Interfaz;
 
 import Dominio.Funcionario;
@@ -14,10 +10,8 @@ import java.util.InputMismatchException;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author yliana
- */
+/** Ventana para ingresar funcionarios.
+ * @author yliana*/
 public class IngresoFuncionario extends javax.swing.JFrame implements PropertyChangeListener {
 
     /**
@@ -233,44 +227,7 @@ public class IngresoFuncionario extends javax.swing.JFrame implements PropertyCh
 
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
         // TODO add your handling code here:
-      //  guardarPersonal();
     }//GEN-LAST:event_formWindowClosing
-
-    /**
-     * @param args the command line arguments
-     */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new IngresoFuncionario().setVisible(true);
-//            }
-//        });
-//    }
-    
 
     private ArrayList<Funcionario> funcionarios;
     private Sistema datos;
@@ -284,6 +241,7 @@ public class IngresoFuncionario extends javax.swing.JFrame implements PropertyCh
     private javax.swing.JTextField txt_edad;
     private javax.swing.JTextField txt_nombre;
     // End of variables declaration//GEN-END:variables
+   
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         limpiarTabla();
