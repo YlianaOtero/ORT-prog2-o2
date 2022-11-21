df392242c0fcfbc9caa0d66628dc339f9e319979
Yliana <ylianaotero@gmail.com>
2022-11-21 00:19:49 -0300

    Emprolijé el código de las ventanas y agregué comentarios descriptivos.

--- src/Interfaz/IngresoDron.java
+++ src/Interfaz/IngresoDron.java
@@ -13,10 +13,8 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author ylian
- */
+/** Ventana para ingresar drones.
+ * @author yliana*/
 public class IngresoDron extends javax.swing.JFrame implements PropertyChangeListener {
 
     /**
@@ -53,6 +51,7 @@ public class IngresoDron extends javax.swing.JFrame implements PropertyChangeLis
         DefaultTableModel modelo = (DefaultTableModel) tbl_datos.getModel();
         modelo.setRowCount(0);
     }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -236,42 +235,7 @@ public class IngresoDron extends javax.swing.JFrame implements PropertyChangeLis
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
         // TODO add your handling code here:
     }//GEN-LAST:event_formWindowClosing
- 
-    
-    /**
-     * @param args the command line arguments
-     */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(IngresoDron.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(IngresoDron.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(IngresoDron.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(IngresoDron.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new IngresoDron().setVisible(true);
-//            }
-//        });
-//    }
+
 
     private ArrayList<Dron> drones;
     private Sistema datos;
--- src/Interfaz/IngresoFuncionario.java
+++ src/Interfaz/IngresoFuncionario.java
@@ -14,10 +14,8 @@ import java.util.InputMismatchException;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author yliana
- */
+/** Ventana para ingresar funcionarios.
+ * @author yliana*/
 public class IngresoFuncionario extends javax.swing.JFrame implements PropertyChangeListener {
 
     /**
@@ -236,41 +234,6 @@ public class IngresoFuncionario extends javax.swing.JFrame implements PropertyCh
       //  guardarPersonal();
     }//GEN-LAST:event_formWindowClosing
 
-    /**
-     * @param args the command line arguments
-     */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(IngresoFuncionario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new IngresoFuncionario().setVisible(true);
-//            }
-//        });
-//    }
-    
     private ArrayList<Funcionario> funcionarios;
     private Sistema datos;
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -283,6 +246,7 @@ public class IngresoFuncionario extends javax.swing.JFrame implements PropertyCh
     private javax.swing.JTextField txt_edad;
     private javax.swing.JTextField txt_nombre;
     // End of variables declaration//GEN-END:variables
+    
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         limpiarTabla();
